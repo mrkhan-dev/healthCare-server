22e8fc1e13fa2ddcca0a5e008f521cdbb9384721
mrkhan-dev <sk5094625@gmail.com>
2024-06-16 23:18:23 +0600

    save user with userInfo to the database

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ app.use(express.json());
 // mongodb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aeb0oh8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-console.log(uri);
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -27,6 +25,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const upcomingTestCollection = client
+      .db("healthCareDB")
+      .collection("upcomingTests");
+    const userCollection = client.db("healthCareDB").collection("users");
+
+    // get upcoming test to homepage
+    app.get("/upcomingTest", async (req, res) => {
+      const result = await upcomingTestCollection.find().toArray();
+      res.send(result);
+    });
+
+    // post user info to database
+    app.post("/userInfo", async (req, res) => {
+      const users = req.body;
+      const query = {email: users.email};
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({message: "this user already exist"});
+      }
+      const result = await userCollection.insertOne(users);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ping: 1});
     console.log(
